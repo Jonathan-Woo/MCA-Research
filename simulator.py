85ac8170313927800f4dc463964957031c5bfe02
Jonathan Woo <jonathan.ck.woo@gmail.com>
2022-06-09 20:14:49 -0400

    Implemented switching behaviour

--- simulator.py
+++ simulator.py
@@ -1,21 +1,38 @@
 import numpy as np
+from random import normalvariate
 
-def rootQuadratic(x, a, b, c):
-    '''Returns y for a quadratic equation in root form parameterized by a and roots b, c
+def normalChoice(lst, mean=None, std=None):
+    '''Returns sampled element from list list with normal distribution
+    parameterized by mean mean and standard deviation std
     '''
-    return a*(x - b)*(x - c)
+    #choose mean to be the middle of the list if not provided
+    if mean == None:
+        mean = (len(lst) - 1)
 
-def vertexQuadratic(x, a, h, k):
-    '''Returns y for a quadratic equation in vertex form parameterized by a and vertex (h, k)
+    if std == None:
+        #CHOOSE DEFAULT STD
+        #std = 
+        pass
+
+    while True:
+        index = int(np.random.normal(mean, std) + 0.5)
+        #check that sampled index is within bounds
+        if 0 <= index < len(lst):
+            return lst[index]
+
+def quadratic(x, a, b, c):
+    '''Returns y for a quadratic equation in standard form parameterized by a,
+    b, and c
     '''
-    return a*(x-h)**2+k
+    return np.multiply(a, x**2) + np.multiply(b, x) + c
 
 def SRCV(y):
     '''Returns SRCV given conductances y
     '''
     sol = 0
-    # for i in range(len(y)-1):
-    #     sol += 
+    #SRCV = difference between adjacent conductance divided by number of points and size of conductance to normalize
+    for i in range(len(y)-1):
+        sol += 
 
 def NLCV(yLeft, yRight):
     '''Returns NLCV given conductances of left and right branches
@@ -51,41 +68,41 @@ def error(aLeft, aRight, pulses):
     return SRCV(yLeft[:-1] + yRight) + NLCV(yLeft, yRight) + ACV(yLeft, yRight)
 
 class memristor:
-    def __init__(self, SRCV, NLCV, ACV, pulses):
+    def __init__(self, SRCV, NLCV, ACV, maxConductance, minConductance, pulses):
         self.SRCV = SRCV
         self.NLCV = NLCV
         self.ACV = ACV
-        # self.maxConductance = maxConductance
-        # self.minConductance = minConductance
+        self.maxConductance = maxConductance
+        self.minConductance = minConductance 
         self.pulses = pulses
     def defineSwitchingBehaviour(self):
         '''Defines quadratic switching look up table of conductance vs number of pulses
         '''
-        #define left quadratic using root form 
-        #constrain left root and right roots
-        #randomize a
-        xLeft = np.arange(self.pulses)
-        aLeft = np.random.random()
-        rootLeft = np.random.random()
-        yLeft = rootQuadratic(xLeft, aLeft, 0, self.pulses)
-        
-        #define right quadratic using vertex form 
-        #constrain vertex to the vertex of the left quadratic
-        #randomize a
-        xRight = np.arange(self.pulses, 1001)
-        aRight = np.random.random()
-        vertexX = self.pulses/2
-        vertexY = yLeft[vertexX]
-        yRight = vertexQuadratic(xRight, aRight, vertexX, vertexY)
-        #truncate yRight to when 0 conductance is reached
-        for i in range(len(yRight)):
-            if yRight[i] < 0:
-                yRight = yRight[:i]
-                break
-        #truncate xRight to the length of yRight
-        xRight = xRight[0:len(yRight)]
+        #choose number of pulses before changing from SET to RESET by sampling
+        #from normal distribution
+        pulsesLst = [*range(self.pulses)]
+        halfSwitching = normalChoice(pulsesLst)
 
-        #minimize error function 
+        #SET goes up to and including half switching
+        pulsesSet = np.arange(halfSwitching + 1)
+        #RESET starts at half switching and stops at self.pulses
+        pulsesReset = np.arange(halfSwitching, self.pulses + 1)
+
+        #define conductance values 
+        '''The math seems complicated by it's built off of the following model:
+        https://www.desmos.com/calculator/1byoj4kgwm
+
+        Since the two quadratic equations are subject to constraints, they can
+        ultimately be written as functions of aSet and aReset alone.
+        '''
+        #initialize random aSet and aReset with constraints
+        aSet = np.random.random() * ((-self.maxConductance + self.minConductance)/halfSwitching**2)
+        conductanceSet = quadratic(pulsesSet, aSet, (self.maxConductance - self.minConductance - aSet*halfSwitching**2)/k, self.minConductance)
+
+        aReset = np.random.random() * (-self.minConductance + self.maxConductance)/(-(halfSwitching-self.pulses)**2)
+        conductanceSet = quadratic(pulsesReset, aReset, (self.minConductance - aReset*self.pulses**2 - self.maxConductance + aReset*halfSwitching**2)/(self.pulses - halfSwitching), self.maxConductance - aReset*halfSwitching**2 - ((self.minConductance - aReset*self.pulses**2 - self.maxConductance + aReset*halfSwitching)/(self.pulses - halfSwitching))*halfSwitching)
+        
+        #optimize aSet and aReset
 
 
     
